bcd29858b6b2b165d790791b6e7c8398f1158c05
CinnamonRollsRoyce <ChristopherTorres171@gmail.com>
2021-08-08 19:43:02 -0700

    Add files via upload

---
+++ covid_19_stats.sql
@@ -0,0 +1,8 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[rolling_vaccination]
+  FROM [Neo_project].[dbo].[percent_pop_vaccinated]
