a84cc10964867208ba063082a27d218c906187ca
CinnamonRollsRoyce <ChristopherTorres171@gmail.com>
2021-08-08 19:46:15 -0700

    Delete covid_19_stats.sql

--- covid_19_stats.sql
+++
@@ -1,8 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [continent]
-      ,[location]
-      ,[date]
-      ,[population]
-      ,[new_vaccinations]
-      ,[rolling_vaccination]
-  FROM [Neo_project].[dbo].[percent_pop_vaccinated]
